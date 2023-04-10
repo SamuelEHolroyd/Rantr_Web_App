2a961b30e1cf4738fa3a9ded376862dddcc502f1
emg00018 <emg00018@mix.wvu.edu>
2023-04-10 15:23:13 -0400

    Create post form

---
+++ Rantr-Web-App/src/app/components/create-thread-form/create-thread-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-thread-form',
+  templateUrl: './create-thread-form.component.html',
+  styleUrls: ['./create-thread-form.component.css']
+})
+export class CreateThreadFormComponent {
+  
+}
