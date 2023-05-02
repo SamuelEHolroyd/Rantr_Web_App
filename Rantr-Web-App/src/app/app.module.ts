4b3c208a9473790ec0052e8e69673604f7abfe2e
MelonCaully <maloneingham@gmail.com>
2023-05-02 16:36:32 -0400

    boop

--- Rantr-Web-App/src/app/app.module.ts
+++ Rantr-Web-App/src/app/app.module.ts
@@ -25,10 +25,9 @@ import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 import { environment } from '../environments/environment';
-import {FormsModule} from "@angular/forms";
+import { FormsModule } from "@angular/forms";
 import { ThreadDisplayCardComponent } from './components/thread-display-card/thread-display-card.component';
 import { SettingsNavbarComponent } from './components/settings-navbar/settings-navbar.component';
-import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
 
 
 @NgModule({
@@ -62,7 +61,6 @@ import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
     AngularFireAuthModule,
     AngularFireStorageModule,
     AngularFireDatabaseModule,
-    AngularFirestoreModule,
     FormsModule
   ],
   providers: [],
