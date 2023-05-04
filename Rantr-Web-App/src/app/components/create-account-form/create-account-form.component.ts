71e7739a703163b05991a03d51cd9e2a65647c15
MelonCaully <maloneingham@gmail.com>
2023-05-03 20:34:16 -0400

    checkpoint

--- Rantr-Web-App/src/app/components/create-account-form/create-account-form.component.ts
+++ Rantr-Web-App/src/app/components/create-account-form/create-account-form.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-account-form',
@@ -9,36 +6,5 @@ import { Router } from '@angular/router';
   styleUrls: ['./create-account-form.component.css'],
 })
 export class CreateAccountFormComponent {
-  constructor(private afAuth: AngularFireAuth, private firestore: AngularFirestore, private router: Router) {}
-
-  async createAccount(
-    email: string,
-    username: string,
-    password: string,
-    confirmPassword: string
-  ) {
-    if (password !== confirmPassword) {
-      alert("Passwords don't match.");
-      return;
-    }
-
-    try {
-      const result = await this.afAuth.createUserWithEmailAndPassword(
-        email,
-        password
-      );
-      if (result.user) {
-        await result.user.updateProfile({ displayName: username });
-
-        // Create a document in Firestore
-        await this.firestore.collection('users').doc(result.user.uid).set({
-          email: email,
-          username: username,
-        });
-      }
-      this.router.navigate(['']);
-    } catch (error: any) {
-      alert(`Error: ${error.message}`);
-    }
-  }
+  
 }
